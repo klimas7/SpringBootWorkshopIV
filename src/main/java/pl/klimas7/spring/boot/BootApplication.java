b694c76adfccc01a6a71576417993a394a9cbbdb
klimas7 <klimas7@gmail.com>
2020-12-04 13:09:59 +0100

    SpringBoot #1: Empty application and dependencies

---
+++ src/main/java/pl/klimas7/spring/boot/BootApplication.java
@@ -0,0 +1,11 @@
+package pl.klimas7.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class);
+    }
+}
